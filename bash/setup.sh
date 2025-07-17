9c7d15c131795e9bfe110a648e930db4121ad731
Ryan <134738730+rhawk117@users.noreply.github.com>
2025-07-17 08:32:29 -0400

    qol: updated setup script

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-echo "Copying .bashrc to ~/.bashrc"
-cp -f ./.bashrc ~/.bashrc
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
 
-echo "Copying .bashrc.d to ~/.bashrc.d"
+
+info() {
+  echo -e "${BLUE}[INFO]${NC} $1"
+}
+
+success() {
+  echo -e "${GREEN}[SUCCESS]${NC} $1"
+}
+
+
+debug() {
+  echo -e "${PURPLE}[DEBUG]${NC} $1"
+}
+
+
+
+debug "Setting up bash environment..."
+
+info "Copying .bashrc to ~/.bashrc"
+cp -rf ./.bashrc ~/.bashrc && {
+  success "Copied .bashrc successfully."
+}
+
 
 if [ ! -d ~/.bashrc.d ]; then
   mkdir -p ~/.bashrc.d
-  echo "Created directory ~/.bashrc.d"
+  info "Created directory ~/.bashrc.d"
 fi
 
-cp -rf ./.bashrc.d ~
+info "Copying files to ~/.bashrc.d"
+cp -rf ./.bashrc.d ~/.bashrc.d && {
+  success "Copied .bashrc.d files successfully."
+}
+
+info "Copying .minttyrc"
+cp -rf ./.minttyrc ~/.minttyrc && {
+  success "Copied .minttyrc successfully."
+}
+
+info "Setup complete. Reload the active shell terminal"
